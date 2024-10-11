5cf81a709751ba6a0d127ffdfb4d944e41e1d6ea
geo <37697824+geomen888@users.noreply.github.com>
2024-10-11 21:51:22 +0200

    Delete backend/dist directory

--- backend/dist/investment/investment.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { InvestmentService } from './investment.service';
-import { InvestmentEntity } from '../postgres/pg-models/investment.entity';
-export declare class InvestmentController {
-    private readonly investmentService;
-    constructor(investmentService: InvestmentService);
-    findAll(): Promise<InvestmentEntity[]>;
-    create(investData: Partial<InvestmentEntity>): Promise<InvestmentEntity>;
-}
--- backend/dist/investment/investment.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { Repository } from 'typeorm';
-import { InvestmentEntity } from '../postgres/pg-models/investment.entity';
-export declare class InvestmentService {
-    private readonly investmentRepository;
-    constructor(investmentRepository: Repository<InvestmentEntity>);
-    findAll(): Promise<InvestmentEntity[]>;
-    create(investmentData: Partial<InvestmentEntity>): Promise<InvestmentEntity>;
-}
